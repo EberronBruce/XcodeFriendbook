8357d8884155a1001cb2c79bb5edf733d1a59ca6
Bruce Burgess <bruceburgess67@yahoo.com>
2016-02-20 13:21:53 +0700

    Remove Xcode generated view Controller and replace it with my own

---
+++ Friendbook/FriendListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FriendListViewController.swift
+//  Friendbook
+//
+//  Created by Bruce Burgess on 2/20/16.
+//  Copyright © 2016 Bruce Burgess. All rights reserved.
+//
+
+import UIKit
+
+class FriendListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
