272098a9be3c63d5207ed19d3e61be13f0284d04
Bruce Burgess <bruceburgess67@yahoo.com>
2016-02-20 15:03:40 +0700

    Connected Tableview, added cells to table view

--- Friendbook/FriendListViewController.swift
+++ Friendbook/FriendListViewController.swift
@@ -8,12 +8,35 @@
 
 import UIKit
 
-class FriendListViewController: UIViewController {
+class FriendListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        /*
+            Set the Table View data source to this view control
+            Set the Table View delegate to this view control
+            This view control will handle the taps and information of the table view
+        */
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        
+        cell.textLabel!.text = "Qhat"
+        
+        return cell
     }
 
 }
